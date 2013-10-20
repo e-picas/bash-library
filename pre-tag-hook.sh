f4dc775bb8e37f3dbeea5572097d6782273a2fc3
Piero Wbmstr <me@e-piwi.fr>
2013-10-20 18:01:34 +0200

    Adding the MarkdownExtended as requirement for dev
    - package added in "require-dev"
    - the "pre-tag-hook" is modified to re-generate the manpage

--- pre-tag-hook.sh
+++ pre-tag-hook.sh
@@ -16,12 +16,40 @@ BRANCH_NAME="$3"
 _VERSION="${TAG_NAME/v/}"
 _DATE=$(git log -1 --format="%ci" --date=short | cut -s -f 1 -d ' ')
 _LIBFILE="src/bash-library.sh"
+_MANFILE="MANPAGE.md"
+_MANMANFILE="src/bash-library.man"
+_MDEBIN="vendor/bin/markdown_extended"
+
+if [ -f "$_MDEBIN" ]; then
+    echo "The binary '$_MDEBIN' can't be found ; the library manpage will not be updated for this tag."
+    echo "If you want to install the markdown tool, run 'composer update --dev' ..."
+    prompt 'Do you want to continue' 'Y/n' 'y'
+    if [ "$USERRESPONSE" != 'y' ]; then exit 0; fi
+fi
 
 if [ -f "$_LIBFILE" ]; then
     sed -i '' -e "s| LIB_VERSION=\".*\"| LIB_VERSION=\"${_VERSION}\"|;s| LIB_DATE=\".*\"| LIB_DATE=\"${_DATE}\"|" "$_LIBFILE";
-    git add "$_LIBFILE" && git commit -m "Automatic version number and date insertion in '$_LIBFILE'"
+    git add "$_LIBFILE"
 else
     verecho "!! > Library file '${_LIBFILE}' not found! (can't update version number and date)"
 fi
+
+if [ -f "$_MANFILE" ]; then
+    sed -i '' -e "s|^Version: .*$|Version: ${_VERSION}|;s|^Date: .*$|Date: ${_DATE}|" "$_MANFILE";
+    git add "$_MANFILE"
+    if [ -f "$_MDEBIN" ]; then
+        "$_MDEBIN" -f man -o "$_MANMANFILE" "$_MANFILE"
+        git add "$_MANMANFILE"
+    else
+        verecho "!! > Binary '${_MDEBIN}' not found! (can't re-generate '${_MANMANFILE}' - try to run 'composer' with '--dev' option)"
+    fi
+else
+    verecho "!! > Manual file '${_MANFILE}' not found! (can't update version number and date)"
+fi
+
+git commit -m "Automatic version number and date insertion" && \
+    LASTSHA=`git log -1 --format="%H"` && \
+    git checkout wip && git cherry-pick $LASTSHA && \
+    git checkout master && git push origin master wip;
 
 # Endfile
