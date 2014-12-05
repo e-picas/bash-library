628114289ad1abba3b17dfab86044d583a83caf3
piwi <me@e-piwi.fr>
2014-12-05 22:44:16 +0100

    new manpage builder

---
+++ build/make-manpage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+_here=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+_source=MANPAGE.md
+_output=src/piwi-bash-library.man
+_mde=${_here}/../modules/markdown-extended/bin/markdown-extended
+
+${_mde} -f man -o "$_output" "${_source}" \
+    && echo "manpage generated in '$_output'" \
+    || echo "an error occurred!" ;
