1529a752f7fbdf795ee6534f3760aa050afac2a5
piwi <me@e-piwi.fr>
2014-12-07 21:53:24 +0100

    updating the documentation builder

--- build/make-documentation.sh
+++ build/make-documentation.sh
@@ -4,14 +4,17 @@ _here=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 _source=src/piwi-bash-library.sh
 _output=DOCUMENTATION.md
 _lib=${_here}/../${_source}
+_type=markdown
+#_type=terminal
 
+# for dev
 #_source=${_here}/../dev/test-doc.sh
-#_output=${_here}/documentation.md # for dev
+#_output=${_here}/documentation.md
 
 source ${_lib} || echo "!! piwi-bash-library not found!" ;
 
 export VERBOSE=true
 
-build_documentation markdown $_output $_source \
+build_documentation "$_type" "$_output" "$_source" \
     && echo "documentation generated in '$_output'" \
     || echo "an error occurred!" ;
